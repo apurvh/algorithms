8ded68ea53fb27eb70eca8dd1e488f0f363fcced
ApurvH <apurv.hajare@gmail.com>
2022-05-24 12:49:01 +0530

    add likedlist cycle

---
+++ leetcode-neetcode150-blind75/141-linked-list-cycle.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let s = head;
+  let f = head;
+  while (f && f.next) {
+    s = s.next;
+    f = f.next.next;
+    if (s === f) return true;
+  }
+  return false;
+};
