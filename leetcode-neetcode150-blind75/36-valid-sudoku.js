f4cb289da5f8a46434c8da0d070efc68e745cd6a
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-03 14:03:56 +0530

    add valid sudoku

---
+++ leetcode-neetcode150-blind75/36-valid-sudoku.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+  // checking duplicates with hashmap
+  const cols = {};
+  const rows = {};
+  const grid3x3 = {};
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const boardVal = board[i][j];
+      const boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+
+      if (boardVal === '.') continue;
+
+      if (
+        (rows[i] || []).includes(boardVal) ||
+        (cols[j] || []).includes(boardVal) ||
+        (grid3x3[boxNum] || []).includes(boardVal)
+      )
+        return false;
+
+      rows[i] = [...(rows[i] || []), boardVal];
+      cols[j] = [...(cols[j] || []), boardVal];
+      grid3x3[boxNum] = [...(grid3x3[boxNum] || []), boardVal];
+    }
+  }
+  return true;
+};
