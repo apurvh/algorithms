4a51e95f9bd233f5afeab9b707b76b7b68cb0868
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-04 09:50:46 +0530

    update leetcode 33

--- leetcode-neetcode150-blind75/33-search-in-rotated-sorted-array.js
+++ leetcode-neetcode150-blind75/33-search-in-rotated-sorted-array.js
@@ -4,26 +4,30 @@
  * @return {number}
  */
 var search = function (nums, target) {
+  /**
+   * Write down test test cases
+   * and handle each
+   */
+
   let l = 0;
   let r = nums.length - 1;
-  /**
-   * TODO: fix the bug
-   */
   while (l <= r) {
     const mid = Math.floor((l + r) / 2);
     if (target === nums[mid]) return mid;
-    // console.log('start', l, r, mid, nums[mid], target)
-    if (target < nums[mid] && target > nums[0]) {
-      r = mid - 1;
-    } else if (target < nums[mid] && target < nums[0]) {
-      l = mid + 1;
-    } else if (target > nums[mid] && target < nums[nums.length - 1]) {
-      l = mid + 1;
-    } else if (target > nums[mid] && target > nums[nums.length - 1]) {
-      r = mid - 1;
+
+    if (nums[l] > nums[mid]) {
+      if (nums[mid] < target && target <= nums[r]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    } else {
+      if (nums[l] <= target && target < nums[mid]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
     }
   }
   return -1;
 };
-
-search([4, 5, 6, 7, 8, 1, 2, 3], 8);
