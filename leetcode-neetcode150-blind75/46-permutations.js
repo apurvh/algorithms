e5d0c7ee53a551be7b2b24b48cdd188f3b73404f
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-04 19:32:24 +0530

    permutations

---
+++ leetcode-neetcode150-blind75/46-permutations.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  const output = [];
+  const recursion = (permutation, set) => {
+    if (set.size === 0) {
+      output.push(permutation);
+      return;
+    }
+    for (let val of set) {
+      const setCopy = new Set(set);
+      setCopy.delete(val);
+      recursion([...permutation, val], setCopy);
+    }
+  };
+  recursion([], new Set(nums));
+  return output;
+};
