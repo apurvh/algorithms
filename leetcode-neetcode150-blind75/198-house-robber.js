b126bf9a542accdd17acc954dcd6415879d34283
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-09 16:06:06 +0530

    add house robbers

---
+++ leetcode-neetcode150-blind75/198-house-robber.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+  if (nums.length < 3) {
+    return Math.max(...nums);
+  }
+
+  if (nums.length === 3) {
+    return Math.max(nums[0] + nums[2], nums[1]);
+  }
+
+  // Bottoms up approach => knowing max that we can rob till index 2
+  // helps us calculate max we can rob at index 0
+  // as we cant rob consecutive houses, we get the following relation
+  for (let i = nums.length - 3; i >= 0; i--) {
+    nums[i] += Math.max(nums[i + 2], nums[i + 3] || 0);
+  }
+
+  // console.log(nums)
+  // return Math.max(nums[0], nums[1])
+  return Math.max(nums[0], nums[1]);
+};
