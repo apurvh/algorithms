d96e54e43dac0cb51bce3a8ff72efb2b63afe6f8
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-13 17:04:52 +0530

    counting bits

---
+++ leetcode-neetcode150-blind75/338-counting-bits.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+  /* brute force, nlogn
+    const output = [0]
+    for(let i=1; i<=n; n++){
+        
+        // count 1's
+        let count = 0
+        let num = i
+        while(num){
+            num = num&(num-1) // TODO why this is not setting num=0 for num=1
+            count++
+        }
+        
+        output.push(count)
+    }
+    
+    return output
+    */
+
+  // O(n)solution using DP
+  // draw out the binary representation of 1-9 and you can see the pattern
+  const dp = new Array(n + 1).fill(0);
+  let offset = 1;
+  for (let i = 1; i <= n; i++) {
+    if (offset * 2 == i) offset = i;
+    dp[i] = 1 + dp[i - offset];
+  }
+
+  return dp;
+};
