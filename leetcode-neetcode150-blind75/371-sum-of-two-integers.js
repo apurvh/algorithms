75ebfb7dcf973789b945a1ad241a19a305de7f84
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-13 21:04:59 +0530

    sum of two integers

---
+++ leetcode-neetcode150-blind75/371-sum-of-two-integers.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function (a, b) {
+  // using combination of XOR and & operations to manipulate bits
+  while (b !== 0) {
+    const temp = (a & b) << 1;
+    a = a ^ b;
+    b = temp;
+  }
+
+  return a;
+};
