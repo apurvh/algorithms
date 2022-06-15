3df7e4c3eeb7baab6ee467990a73ff4631a0e5e9
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-15 10:16:06 +0530

    simplify decode ways code

--- leetcode-neetcode150-blind75/91-decode-ways.js
+++ leetcode-neetcode150-blind75/91-decode-ways.js
@@ -3,40 +3,13 @@
  * @return {number}
  */
 var numDecodings = function (s) {
-  const dp = [];
+  const dp = new Array(s.length + 2);
 
-  /**
-   * calculate dp[n] & dp[n-1] .. n being the len of s
-   */
-  // 12 -> 21
-  // 02 -> 01
-  // 20 -> 10
-  // 30 -> 00
-  if (s[s.length - 1] === '0') dp[s.length - 1] = 0;
-  else dp[s.length - 1] = 1;
+  // adding these two values simplifies the edgecase
+  dp[dp.length - 1] = 0;
+  dp[dp.length - 2] = 1;
 
-  // 01-09
-  if (s[s.length - 2] === '0') dp[s.length - 2] = 0;
-  // 30-99
-  else if ('3456789'.includes(s[s.length - 2])) {
-    dp[s.length - 2] = dp[s.length - 1];
-  }
-  // 10-19
-  else if (s[s.length - 2] === '1') {
-    dp[s.length - 2] = dp[s.length - 1] + 1;
-  } else {
-    // 20-26
-    if ('0123456'.includes(s[s.length - 1])) {
-      dp[s.length - 2] = dp[s.length - 1] + 1;
-    }
-    // 27-29
-    else {
-      dp[s.length - 2] = dp[s.length - 1];
-    }
-  }
-
-  // calculate dp[n-3] to dp[0]
-  for (i = s.length - 3; i >= 0; i--) {
+  for (i = s.length - 1; i >= 0; i--) {
     if (s[i] === '0') dp[i] = 0;
     else dp[i] = dp[i + 1];
 
@@ -44,7 +17,5 @@ var numDecodings = function (s) {
     if (s[i] === '1') dp[i] += dp[i + 2];
     if (s[i] === '2' && '0123456'.includes(s[i + 1])) dp[i] += dp[i + 2];
   }
-
-  console.log(dp);
   return dp[0];
 };
