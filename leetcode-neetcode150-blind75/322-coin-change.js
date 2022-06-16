570f6c9eff9f0056c4ac192043f18fb407f6b460
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-16 23:31:24 +0530

    add coin change

---
+++ leetcode-neetcode150-blind75/322-coin-change.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = new Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
+
+  for (let i = 1; i < amount + 1; i++) {
+    for (const coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+      }
+    }
+  }
+
+  if (dp[amount] !== amount + 1) {
+    return dp[amount];
+  } else {
+    return -1;
+  }
+};
