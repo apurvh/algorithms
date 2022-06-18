3c9c4d1f7ff6b228f040ea5559739a5ece1a05bb
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-18 17:43:38 +0530

    clean up word search and make it more efficient

--- leetcode-neetcode150-blind75/79-word-search.js
+++ leetcode-neetcode150-blind75/79-word-search.js
@@ -4,55 +4,51 @@
  * @return {boolean}
  */
 var exist = function (board, word) {
-  let output = false;
-
-  const getPosId = (i, j) => {
-    return i * board[0].length + j;
-  };
-
-  const dfs = (i, j, index, set) => {
-    const boardPos = board[i][j];
-    if (boardPos !== word[index]) return;
+  // Two strategies to deal with duplicates
+  // 1. using set - add and remove position ids from set
+  // 2. replace char at i,j with # and then reassign after dfs complete
+  /*
+    let set = new Set()
+    const getPosId = (i,j) =>{
+        return (i * board[0].length + j)
+    }
+    */
+  const dfs = (i, j, index) => {
+    if (index === word.length) return true;
+    // cases to terminate DFS
+    // 1. out of bounds
+    // 2. revisiting already visited
+    // 3. words are different
+    if (i > board.length - 1 || i < 0) return false;
+    if (j > board[0].length - 1 || j < 0) return false;
+    // if(set.has(getPosId(i,j))) return false
+    if (board[i][j] === '#') return false;
+    if (board[i][j] !== word[index]) return false;
 
     // using set to track which pos are already visited
-    set.add(getPosId(i, j));
+    // set.add(getPosId(i,j))
+    const char = board[i][j];
+    board[i][j] = '#';
 
-    if (boardPos === word[index] && index === word.length - 1) {
-      output = true;
-      return;
-    }
+    const res =
+      dfs(i + 1, j, index + 1, set) ||
+      dfs(i - 1, j, index + 1, set) ||
+      dfs(i, j + 1, index + 1, set) ||
+      dfs(i, j - 1, index + 1, set);
 
-    if (boardPos === word[index]) {
-      // const forward = [i, j+1]
-      if (j + 1 < board[0].length && !set.has(getPosId(i, j + 1))) {
-        dfs(i, j + 1, index + 1, new Set(set));
-      }
-
-      // const backward = [i, j-1]
-      if (j - 1 >= 0 && !set.has(getPosId(i, j - 1))) {
-        dfs(i, j - 1, index + 1, new Set(set));
-      }
-
-      // const down = [i+1, j]
-      if (i + 1 < board.length && !set.has(getPosId(i + 1, j))) {
-        dfs(i + 1, j, index + 1, new Set(set));
-      }
-
-      // const up = [i-1, j]
-      if (i - 1 >= 0 && !set.has(getPosId(i - 1, j))) {
-        dfs(i - 1, j, index + 1, set);
-      }
-    }
+    // set.delete(getPosId(i,j))
+    board[i][j] = char;
+    return res;
   };
 
+  // check board for first letter matches
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
       const val = board[i][j];
       if (val === word[0]) {
-        dfs(i, j, 0, new Set());
+        if (dfs(i, j, 0)) return true;
       }
     }
   }
-
-  return output;
+  return false;
 };
