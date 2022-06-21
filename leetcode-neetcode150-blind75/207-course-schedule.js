b34f670786e8b0f3a4ee033ea40a413cc9f542a8
Apurv Hajare <apurv.hajare@gmail.com>
2022-06-21 10:03:24 +0530

    update course schedule

--- leetcode-neetcode150-blind75/207-course-schedule.js
+++ leetcode-neetcode150-blind75/207-course-schedule.js
@@ -4,44 +4,34 @@
  * @return {boolean}
  */
 var canFinish = function (numCourses, prerequisites) {
-  const map = new Map(); // map of a course and which courses is it prereq of
-
-  // get the map
-  for (const pair of prerequisites) {
-    const course = pair[0];
-    const prereq = pair[1];
-    map.set(prereq, [...(map.get(prereq) || []), course]);
+  // create adjacency map
+  const map = new Map();
+  for (let i = 0; i < prerequisites.length; i++) {
+    // 0 - crs, 1 - pre
+    const crs = prerequisites[i][0];
+    const pre = prerequisites[i][1];
+    map.set(crs, [...(map.get(crs) || []), pre]);
   }
 
-  // console.log(map)
-
-  let visited = new Set();
-  let output = true;
-
-  // dfs visit nodes and detect cycle with the help of visited
-  const dfs = (curr) => {
-    // console.log(curr, visited)
-    if (visited.has(curr)) {
-      output = false;
-      return;
-    }
-
+  // dfs to detect cycle in the graph
+  const visited = new Set();
+  const hasCycleDFS = (curr) => {
+    if (visited.has(curr)) return true;
     visited.add(curr);
-
-    const courses = map.get(curr);
-    if (courses) {
-      for (const course of courses) {
-        dfs(course);
+    const children = map.get(curr);
+    if (children) {
+      for (let i = 0; i < children.length; i++) {
+        // return true if we detect cycle
+        if (hasCycleDFS(children[i])) return true;
       }
     }
     visited.delete(curr);
-    map.set(curr, []);
+    // remove already visisted node
+    map.delete(curr);
   };
-
-  for (const [key, val] of map) {
-    dfs(key);
-    // visited = new Set()
+  for (const [course, req] of map) {
+    // console.log(course)
+    if (hasCycleDFS(course)) return false;
   }
-
-  return output;
+  return true;
 };
